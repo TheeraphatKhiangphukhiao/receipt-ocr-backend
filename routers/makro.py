11d006e8682f136efb2ea8ecc741af29f6e986aa
TheeraphatKhiangphukhiao <zerostar02yi@gmail.com>
2024-06-23 13:54:49 +0700

    Edit the makro.py file

--- routers/makro.py
+++ routers/makro.py
@@ -31,18 +31,33 @@ async def read_makro():
 async def extract_makro_receipt_information(file: UploadFile):
     imRGB = await create_upload_file(file) #ส่งไฟล์ไปยังฟังก์ชั่น
     imGray = await convert_to_grayscale(imRGB) #ส่งรูปภาพ RGB ไปยังฟังก์ชั่นเพื่อเเปลงเป็นภาพระดับเทา
-
-    imGray = cv.equalizeHist(imGray)
     
-    scale_percent = 150
-    width = int(imGray.shape[1] * scale_percent / 100)
-    height = int(imGray.shape[0] * scale_percent / 100)
-    rescaled = cv.resize(imGray, (width, height), interpolation=cv.INTER_LINEAR)
-
-    blur = cv.GaussianBlur(rescaled, (5, 5), 0)
+    #เเปลงภาพระดับเทาให้เป็นภาพเเบบ binary
+    #THRESH_BINARY_INV : เเปลงพิกเซลที่มีค่าน้อยกว่า threshold ให้เป็นสีขาว 255 และพิกเซลที่มีค่ามากกว่า threshold ให้เป็นสีดำ 0
+    #THRESH_OTSU : คำนวณหา threshold โดยวิธี Otsu
+    thresh = cv.threshold(imGray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1] #คำตอบที่ได้จะเป็น tuple ที่ประกอบด้วยค่า threshold และภาพที่ผ่านการ threshold ซึ่งเราสนใจแค่ภาพเลยใช้ [1] เพื่อเลือกเฉพาะภาพนั้นมาใช้งาน
+    print(thresh)
     
-    cv.namedWindow('blur', cv.WINDOW_NORMAL)
-    cv.imshow('blur', blur) # คำสั่งเเสดงผลภาพ
+    noise_reduced = cv.medianBlur(thresh, 3) #ทำการลด noise โดยใช้ฟิลเตอร์แบบ median blur กับภาพเเละใช้ kernel 3x3
 
-    cv.waitKey(0) # คำสั่งรอคอยการกด Keyboard
-    cv.destroyAllWindows() # เป็นการล้างหน้าต่างทั้งหมดที่เปิดเเสดงผลภาพ 
+    #เพิ่มความคมชัด
+    sharpened = cv.addWeighted(noise_reduced, 1.5, noise_reduced, -0.5, 0)
+
+    #ทำการเพิ่มขนาดของรูปภาพให้ใหญ่ขึ้น 1.5 เท่าในเเกน x เเละ 1.5 เท่าในเเกน y
+    #interpolation=cv.INTER_LINEAR : ใช้การ interpolation แบบ linear เพื่อเพิ่มความละเอียดของภาพ
+    resized = cv.resize(sharpened, None, fx=1.5, fy=1.5, interpolation=cv.INTER_LINEAR)
+    
+    # cv.namedWindow('resized', cv.WINDOW_NORMAL)
+    # cv.imshow('resized', resized) # คำสั่งเเสดงผลภาพ
+    # cv.waitKey(0) # คำสั่งรอคอยการกด Keyboard
+    # cv.destroyAllWindows() # เป็นการล้างหน้าต่างทั้งหมดที่เปิดเเสดงผลภาพ 
+    
+    text = pytesseract.image_to_string(resized, lang='Tha+Eng') #เเปลงรูปภาพใบเสร็จไปเป็น text
+    text = text.splitlines() #เเบ่งบรรทัดตามการขึ้นบรรทัดใหม่ \n
+    
+    for index in range(len(text)):
+        if re.compile(r'^\d+\s+\d{13}').search(text[index]):
+            print(text[index])
+        elif re.compile(r'ชำระโดย').search(text[index]):
+            print(text[index])
+            break
