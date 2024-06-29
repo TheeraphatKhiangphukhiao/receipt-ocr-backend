a12b7ab3372800b13727d9d92900f10242f757d6
TheeraphatKhiangphukhiao <zerostar02yi@gmail.com>
2024-06-29 13:27:17 +0700

    Edit render.yaml

--- setup_ocr.py
+++ setup_ocr.py
@@ -1,3 +1,3 @@
 import pytesseract #เพื่อเเปลงรูปภาพใบเสร็จรับเงินมาเป็น text
 
-pytesseract.pytesseract.tesseract_cmd = r'modules\Tesseract-OCR\tesseract.exe' #กำหนดเส้นทางไปยังที่อยู่ของ tesseract-ocr เพื่อนำมาใช้งาน
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract' #กำหนดเส้นทางไปยังที่อยู่ของ tesseract-ocr เพื่อนำมาใช้งาน
