09938af6c9b48e176b6df8f0c5a6a1e15f9ffef7
TheeraphatKhiangphukhiao <zerostar02yi@gmail.com>
2024-07-14 09:39:37 +0700

    Add Middleware

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ app = FastAPI() #สร้าง instance ของ FastAPI application
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], #อนุญาตทุกๆ โดเมนให้เข้าถึงข้อมูล
+    allow_origins=["http://localhost:5173"], #อนุญาตทุกๆ โดเมนให้เข้าถึงข้อมูล
     allow_credentials=True, #อนุญาตให้ส่ง cookies และ headers ที่เกี่ยวข้องกับ credentials กลับไปยังเซิร์ฟเวอร์
     allow_methods=["*"], #อนุญาตการใช้งานทุก method ใน HTTP request
     allow_headers=["*"], #อนุญาตทุก header ใน HTTP request
 )
 
+
 #กำหนดเส้นทาง routers
 app.include_router(index_router, prefix="/index") #เส้นทางทั้งหมดใน index_router จะถูกเรียกผ่าน URL ที่ขึ้นต้นด้วย "/index"
 app.include_router(upload_router, prefix="/upload") #เส้นทางทั้งหมดใน upload_router จะถูกเรียกผ่าน URL ที่ขึ้นต้นด้วย "/upload"
+
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_root():
